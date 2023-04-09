5cc97a5ba0a5a8df3d1bc851ccec911dd5ce8b8c
Amadeus Kiener <a.kiener@unibrand.de>
2023-04-09 18:26:38 +0200

    improve backend preview for PageMenu Plugin elements

--- puck/Classes/Preview/PuckPreviewRenderer.php
+++ puck/Classes/Preview/PuckPreviewRenderer.php
@@ -9,6 +9,8 @@ use TYPO3\CMS\Core\Service\FlexFormService;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 use TYPO3\CMS\Backend\Preview\PreviewRendererInterface;
 use TYPO3\CMS\Backend\View\BackendLayout\Grid\GridColumnItem;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Backend\Routing\UriBuilder;
 
 use B13\Container\Backend\Preview\ContainerPreviewRenderer;
 use B13\Container\Tca\Registry;
@@ -38,17 +40,18 @@ class PuckPreviewRenderer implements PreviewRendererInterface
             $containerPreview = $containerPreviewRenderer->renderPageModulePreviewContent($item);
         }
 
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName('EXT:puck/Resources/Private/Fluid/Backend/Templates/ContentPreview/Content.html'));
+        $view->setPartialRootPaths(['EXT:puck/Resources/Private/Fluid/Backend/Partials/ContentPreview/']);
+
         // add flexform data to the record
         if ($record['pi_flexform']) {
             $flexformService = GeneralUtility::makeInstance(FlexFormService::class);
             $flexform = $flexformService->convertFlexFormContentToArray($record['pi_flexform']);
             $record['pi_flexform'] = $flexform;
+            $view->assign('processedMenuData', $this->getDataForPageMenuFlexFormPreview($flexform));
         }
         $item->setRecord($record);
-
-        $view = GeneralUtility::makeInstance(StandaloneView::class);
-        $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName('EXT:puck/Resources/Private/Fluid/Backend/Templates/ContentPreview/Content.html'));
-        $view->setPartialRootPaths(['EXT:puck/Resources/Private/Fluid/Backend/Partials/ContentPreview/']);
         $view->assign('item', $item);
 
         return $view->render() . $containerPreview;
@@ -67,6 +70,67 @@ class PuckPreviewRenderer implements PreviewRendererInterface
     public function wrapPageModulePreview(string $header, string $content, GridColumnItem $item): string
     {
         return $header . $content;
+    }
+
+    protected function getDataForPageMenuFlexFormPreview($flexform): array
+    {
+        $processedMenuData = [];
+        $uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
+        foreach(['pages', 'parents'] as $key) {
+            if (isset($flexform['settings'][$key]) && !empty($flexform['settings'][$key])) {
+                $uids = explode(',', $flexform['settings'][$key]);
+                foreach ($uids as $uid) {
+                    $page = BackendUtility::getRecord('pages', $uid, 'title,uid', '', true);
+                    $page['backend_link'] = $uriBuilder->buildUriFromRoute(
+                        'web_layout',
+                        ['id' => $page['uid']]
+                    );
+                    $page['table'] = 'pages';
+                    $page['backend_link_title'] = 'Switch to this page';
+                    $processedMenuData[$key][] = $page;
+                }
+            }
+        }
+        $recordGroups = [
+          'author' => [
+            'table' => 'tx_puck_domain_model_person',
+            'title' => 'Author',
+            'titleField' => 'name',
+              'uids' => $flexform['settings']['demand']['author']
+          ],
+            'categories' => [
+                'table' => 'sys_category',
+                'title' => 'Categories',
+                'titleField' => 'title',
+                'uids' => $flexform['settings']['demand']['category']['list']
+            ],
+            'filter_categories' => [
+                'table' => 'sys_category',
+                'title' => 'Categories in filter',
+                'titleField' => 'title',
+                'uids' => $flexform['settings']['template']['filterCategories']
+            ],
+        ];
+        foreach($recordGroups as $key => $recordGroup) {
+            if (isset($recordGroup['uids']) && !empty($recordGroup['uids'])) {
+                $uids = explode(',', $recordGroup['uids']);
+                foreach ($uids as $uid) {
+                    $record = BackendUtility::getRecord($recordGroup['table'], $uid, 'uid,' . $recordGroup['titleField'], '', true);
+                    $record['backend_link'] = $uriBuilder->buildUriFromRoute(
+                        'record_edit',
+                        [
+                            'edit' => [$recordGroup['table'] => [$uid => 'edit']],
+                            'returnUrl' => GeneralUtility::getIndpEnv('REQUEST_URI'),
+                        ]
+                    );
+                    $record['table'] = $recordGroup['table'];
+                    $record['title'] = $record[$recordGroup['titleField']];
+                    $record['backend_link_title'] = 'Edit record';
+                    $processedMenuData[$key][] = $record;
+                }
+            }
+        }
+        return $processedMenuData;
     }
 
 }
