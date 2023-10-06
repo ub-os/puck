c35728a1497767464b153694874a81b5b94dbd0a
Amadeus Kiener <a.kiener@unibrand.de>
2023-10-06 13:54:38 +0200

    remove deprecated from  ContentRepository

--- puck/Classes/Domain/Repository/ContentRepository.php
+++ puck/Classes/Domain/Repository/ContentRepository.php
@@ -2,14 +2,14 @@
 
 namespace UBOS\Puck\Domain\Repository;
 
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Object\ObjectManager;
 
 class ContentRepository extends Repository
 {
     public function initializeObject() {
-        $querySettings = $this->objectManager->get(\TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings::class);
+        $querySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
         $querySettings->setRespectStoragePage(false);
         $this->setDefaultQuerySettings($querySettings);
     }
