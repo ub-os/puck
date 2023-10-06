263372abd917934910e1dab74d273c6d5c4b994c
Amadeus Kiener <a.kiener@unibrand.de>
2023-10-06 17:05:16 +0200

    fix container elements showitems bug

--- puck/Configuration/TCA/Overrides/tt_content.php
+++ puck/Configuration/TCA/Overrides/tt_content.php
@@ -1,19 +1,20 @@
 <?php
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use UBOS\Puck\Preview\PuckPreviewRenderer;
 use UBOS\Puck\Utility\PuckUtility;
 use UBOS\Puckloader\Loader;
-
 
 $contentTcaPath = ExtensionManagementUtility::extPath('puck', 'Configuration/TCA/Content');
 
 $columns = require $contentTcaPath . '/columns.php';
 $palettes = require $contentTcaPath . '/palettes.php';
 
+Loader::loadTca('puck');
+
 $typeNames = PuckUtility::getBaseFilesInDir($contentTcaPath . '/Types/', 'php');
 foreach ($typeNames as $type) {
     require $contentTcaPath . '/Types/' . $type . '.php';
-    //$GLOBALS['TCA']['tt_content']['types'][$type]['previewRenderer'] = PuckPreviewRenderer::class;
+    $GLOBALS['TCA']['tt_content']['types'][$type]['previewRenderer'] = PuckPreviewRenderer::class;
 }
 
-Loader::loadTca('puck');
