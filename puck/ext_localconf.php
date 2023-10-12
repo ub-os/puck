2fe2857909947192f9324136f8e825586bf96955
Amadeus Kiener <a.kiener@unibrand.de>
2023-10-12 13:24:48 +0200

    register be-ck-contents.css via ext_localconf.php instead of RTE config yaml

--- puck/ext_localconf.php
+++ puck/ext_localconf.php
@@ -54,3 +54,5 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['backendLogo'] = 'EXT:puck/
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['backendFavicon'] = 'EXT:puck/Resources/Public/Icons/Favicons/packages/default/favicon.ico';
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginHighlightColor'] = '#A46CDC';
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginLogo'] = 'EXT:puck/Resources/Public/Icons/Logos/default.svg';
+
+$GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['ckeditor_puck'] = 'EXT:puck/Resources/Public/Css/be-ck-contents.css';
