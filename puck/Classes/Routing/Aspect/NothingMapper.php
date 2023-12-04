587b1cad750c721b7ae1cdefb92a45801554b298
Amadeus Kiener <a.kiener@unibrand.de>
2023-12-04 16:29:58 +0100

    make assets minification/compression dependent on applicationContext

---
+++ puck/Classes/Routing/Aspect/NothingMapper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UBOS\Puck\Routing\Aspect;
+
+use TYPO3\CMS\Core\Routing\Aspect\StaticMappableAspectInterface;
+
+class NothingMapper implements StaticMappableAspectInterface
+{
+    public function generate(string $value): ?string
+    {
+        return $value;
+    }
+    public function resolve(string $value): ?string
+    {
+        return $value;
+    }
+}
