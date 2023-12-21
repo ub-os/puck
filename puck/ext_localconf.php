1de8b16dc19d680c2304419d207bbd123bb149b3
Amadeus Kiener <a.kiener@unibrand.de>
2023-12-21 17:10:42 +0100

    enable doktype_typolink middleware

--- puck/ext_localconf.php
+++ puck/ext_localconf.php
@@ -52,9 +52,12 @@ $GLOBALS['TYPO3_CONF_VARS']['BE']['compressionLevel'] = 9;
 $GLOBALS['TYPO3_CONF_VARS']['FE']['compressionLevel'] = 9;
 
 // Backend Extension Configuration
+$GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['ckeditor_puck'] = 'EXT:puck/Resources/Public/Css/be-ck-contents.css';
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['backendLogo'] = 'EXT:puck/Resources/Public/Icons/Favicons/packages/default/android-chrome-72x72.png';
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['backendFavicon'] = 'EXT:puck/Resources/Public/Icons/Favicons/packages/default/favicon.ico';
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginHighlightColor'] = '#A46CDC';
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginLogo'] = 'EXT:puck/Resources/Public/Icons/Logos/default.svg';
 
-$GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['ckeditor_puck'] = 'EXT:puck/Resources/Public/Css/be-ck-contents.css';
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['doktype_typolink']['enableMiddleware'] = true;
+
+
