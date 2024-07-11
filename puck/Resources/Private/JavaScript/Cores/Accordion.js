5634db4730aa9f2474a180a04845455f76e3a90b
Amadeus Kiener <a.kiener@unibrand.de>
2024-07-11 21:19:05 +0200

    rework accordion for with <details> tag

--- puck/Resources/Private/JavaScript/Cores/Accordion.js
+++ puck/Resources/Private/JavaScript/Cores/Accordion.js
@@ -19,11 +19,13 @@ export default class Accordion extends Toggleable {
     toggleOn(transition= true) {
         super.toggleOn(transition);
         this.el.style[this.useMinHeight ? 'minHeight' : 'height'] = `${(this.el.scrollHeight).toString()}px`
+        this.el.open = true
         this.toggleUnits.forEach(t => t.ariaExpanded = 'true')
     }
     toggleOff(transition= true, changeUrlHash =  true) {
         super.toggleOff(transition, changeUrlHash)
-        this.el.style[this.useMinHeight ? 'minHeight' : 'height'] = `0`
+        this.el.style[this.useMinHeight ? 'minHeight' : 'height'] = `${this.el.$('summary')?.offsetHeight ?? '0'}px`
+        this.el.open = false
         this.toggleUnits.forEach(t => t.ariaExpanded = 'false')
     }
     connect() {
