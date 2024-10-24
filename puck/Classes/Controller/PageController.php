e58c37b560119d45adb7728a7104cf5b25c1946e
Amadeus Kiener <a.kiener@unibrand.de>
2024-10-24 23:51:49 +0200

    dev

--- puck/Classes/Controller/PageController.php
+++ puck/Classes/Controller/PageController.php
@@ -93,19 +93,19 @@ class PageController extends ActionController
             ->set('template', $siteSettings->get('template'))
             ->set('navigation', $siteSettings->get('navigation'))
             ->set('doktypes', $siteSettings->get('doktypes'));
-        $this->pageRenderer->addHeaderData($this->getFaviconHtml($siteSettings));
+        $this->pageRenderer->addHeaderData($this->getFaviconHeadTags($siteSettings));
         $this->view->setTemplateRootPaths([$this->settings['view']['templateRootPath']]);
         $this->view->assignMultiple($variables);
         return $this->htmlResponse();
     }
 
 
-    protected function getFaviconHtml($siteSettings): string
+    protected function getFaviconHeadTags($siteSettings): string
     {
         $faviconPath = PathUtility::getAbsoluteWebPath(GeneralUtility::getFileAbsFileName(
             'EXT:puck/Resources/Public/Icons/Favicons/packages/'
             . $siteSettings->get('template.favicon')
         ));
-        return '<link rel="icon" type="image/x-icon" href="' . $faviconPath . '/favicon.ico"><link rel="icon" type="image/png" sizes="16x16" href="' . $faviconPath . '/favicon-16x16.png"><link rel="icon" type="image/png" sizes="32x32" href="' . $faviconPath . '/favicon-32x32.png"><link rel="icon" type="image/png" sizes="48x48" href="' . $faviconPath . '/favicon-48x48.png"><link rel="manifest" href="' . $faviconPath . '/manifest.webmanifest"><meta name="mobile-web-app-capable" content="yes"><meta name="theme-color" content="#fff"><link rel="apple-touch-icon" sizes="57x57" href="' . $faviconPath . '/apple-touch-icon-57x57.png"><link rel="apple-touch-icon" sizes="60x60" href="' . $faviconPath . '/apple-touch-icon-60x60.png"><link rel="apple-touch-icon" sizes="72x72" href="' . $faviconPath . '/apple-touch-icon-72x72.png"><link rel="apple-touch-icon" sizes="76x76" href="' . $faviconPath . '/apple-touch-icon-76x76.png"><link rel="apple-touch-icon" sizes="114x114" href="' . $faviconPath . '/apple-touch-icon-114x114.png"><link rel="apple-touch-icon" sizes="120x120" href="' . $faviconPath . '/apple-touch-icon-120x120.png"><link rel="apple-touch-icon" sizes="144x144" href="' . $faviconPath . '/apple-touch-icon-144x144.png"><link rel="apple-touch-icon" sizes="152x152" href="' . $faviconPath . '/apple-touch-icon-152x152.png"><link rel="apple-touch-icon" sizes="167x167" href="' . $faviconPath . '/apple-touch-icon-167x167.png"><link rel="apple-touch-icon" sizes="180x180" href="' . $faviconPath . '/apple-touch-icon-180x180.png"><link rel="apple-touch-icon" sizes="1024x1024" href="' . $faviconPath . '/apple-touch-icon-1024x1024.png"><meta name="apple-mobile-web-app-capable" content="yes"><meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">';
+        return '<link rel="icon" type="image/x-icon" href="' . $faviconPath . '/favicon.ico"><link rel="icon" type="image/png" sizes="16x16" href="' . $faviconPath . '/favicon-16x16.png"><link rel="icon" type="image/png" sizes="32x32" href="' . $faviconPath . '/favicon-32x32.png"><link rel="icon" type="image/png" sizes="48x48" href="' . $faviconPath . '/favicon-48x48.png"><meta name="theme-color" content="#fff"><link rel="apple-touch-icon" sizes="57x57" href="' . $faviconPath . '/apple-touch-icon-57x57.png"><link rel="apple-touch-icon" sizes="60x60" href="' . $faviconPath . '/apple-touch-icon-60x60.png"><link rel="apple-touch-icon" sizes="72x72" href="' . $faviconPath . '/apple-touch-icon-72x72.png"><link rel="apple-touch-icon" sizes="76x76" href="' . $faviconPath . '/apple-touch-icon-76x76.png"><link rel="apple-touch-icon" sizes="114x114" href="' . $faviconPath . '/apple-touch-icon-114x114.png"><link rel="apple-touch-icon" sizes="120x120" href="' . $faviconPath . '/apple-touch-icon-120x120.png"><link rel="apple-touch-icon" sizes="144x144" href="' . $faviconPath . '/apple-touch-icon-144x144.png"><link rel="apple-touch-icon" sizes="152x152" href="' . $faviconPath . '/apple-touch-icon-152x152.png"><link rel="apple-touch-icon" sizes="167x167" href="' . $faviconPath . '/apple-touch-icon-167x167.png"><link rel="apple-touch-icon" sizes="180x180" href="' . $faviconPath . '/apple-touch-icon-180x180.png"><link rel="apple-touch-icon" sizes="1024x1024" href="' . $faviconPath . '/apple-touch-icon-1024x1024.png">';
     }
 }
