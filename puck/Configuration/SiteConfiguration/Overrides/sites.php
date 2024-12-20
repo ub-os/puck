d707a809e4be415ffa89b932d0f8272e069fbd92
Amadeus Kiener <a.kiener@unibrand.de>
2024-12-20 16:38:19 +0100

    extend site configuration form with limitToPages for route enhancers

---
+++ puck/Configuration/SiteConfiguration/Overrides/sites.php
@@ -0,0 +1,20 @@
+<?php
+$GLOBALS['SiteConfiguration']['site']['columns']['route_pageMenu_limitToPages'] = [
+    'label' => 'Page Menu pages',
+    'description' => 'Comma separated list of page uids where the route enhancer should be active',
+    'config' => [
+        'type' => 'input',
+    ]
+];
+$GLOBALS['SiteConfiguration']['site']['columns']['route_indexedSearch_limitToPages'] = [
+    'label' => 'Indexed Search pages',
+    'description' => 'Comma separated list of page uids where the route enhancer should be active',
+    'config' => [
+        'type' => 'input',
+    ]
+];
+$GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] = str_replace(
+    ', routes',
+    ', routes, --div--;Route Enhancers, route_pageMenu_limitToPages , route_indexedSearch_limitToPages', $GLOBALS['SiteConfiguration']['site']['types']['0']['showitem']
+);
+
