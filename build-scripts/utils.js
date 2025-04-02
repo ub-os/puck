3d0ba1325385aaa0fef197e818c5a233f1886aac
Amadeus Kiener <a.kiener@unibrand.de>
2025-04-02 14:38:07 +0200

    dev

--- build-scripts/utils.js
+++ build-scripts/utils.js
@@ -16,7 +16,7 @@ const log = {
 	success: (msg) => console.log(chalk.green(`✓ ${msg}`)),
 	warning: (msg) => console.log(chalk.yellow(`⚠ ${msg}`)),
 	error: (msg) => console.log(chalk.red(`✗ ${msg}`)),
-	header: (msg) => console.log(chalk.bold.cyan(`\n=== ${msg} ===`))
+	header: (msg) => console.log(chalk.inverse(`\n▓▓▓▓▓▓▓▓▓▓ ${msg} ▓▓▓▓▓▓▓▓▓▓`))
 };
 
 export { ensureDirectoryExistence, log }
