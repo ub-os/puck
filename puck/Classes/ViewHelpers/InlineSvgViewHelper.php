ba011138b79bb743260e3eb880eb37ba19cc9e86
Amadeus Kiener <a.kiener@unibrand.de>
2025-10-10 14:42:49 +0200

    add InlineSvgViewHelper, optimze element:media component

---
+++ puck/Classes/ViewHelpers/InlineSvgViewHelper.php
@@ -0,0 +1,41 @@
+<?php
+namespace UBOS\Puck\ViewHelpers;
+
+use TYPO3\CMS\Core\Resource\FileInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class InlineSvgViewHelper extends AbstractViewHelper
+{
+	protected $escapeOutput = false;
+
+	public function initializeArguments()
+	{
+		$this->registerArgument('file', FileInterface::class, 'File reference', true);
+		$this->registerArgument('class', 'string', 'class', false, '');
+	}
+
+	public function render()
+	{
+		$file = $this->arguments['file'];
+		$class = $this->arguments['class'];
+
+		if ($file->getExtension() !== 'svg') {
+			return '';
+		}
+
+		$svgContent = $file->getContents();
+
+		// remove invalid html tags
+		$svgContent = preg_replace('/<\?xml.*?\?>/', '', $svgContent);
+		// remove comments
+		$svgContent = preg_replace('/<!--.*?-->/s', '', $svgContent);
+		// insulate ids
+		$svgContent = preg_replace('/id="([^"]+)"/', 'id="' . uniqid('inline-svg-') . '"', $svgContent);
+		// Add class attribute if provided
+		if ($class) {
+			$svgContent = preg_replace('/(<svg[^>]*)(>)/', '$1 class="' . htmlspecialchars($class) . '"$2', $svgContent);
+		}
+
+		return $svgContent;
+	}
+}
